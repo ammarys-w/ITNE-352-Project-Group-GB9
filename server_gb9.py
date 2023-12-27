5f93ebaab040d5754aacc3b11aed4731459efa58
ammarys-w <ammar39876@gmail.com>
2023-12-27 15:08:02 +0300

    upload first function on server side

--- server_gb9.py
+++ server_gb9.py
@@ -2,15 +2,23 @@ import socket,json
 import sys
 api_key = "8d782c20ade5ab1413c59a3f9b545516"
 IP="192.168.8.101"
-port=40000
+port=50000
 
 # Getting data from the WEbsite
-def get_data(arr_icao):
-    link = f"http://api.aviationstack.com/v1/flights?access_key={api_key}&arr_icao={arr_icao}&limit=100"
-    response = response.get(link)
-    flight_data = response.json()
+def retrieve_data(arr_icao):
+    url = f"http://api.aviationstack.com/v1/flights?access_key={api_key}&arr_icao={arr_icao}&limit=100"
+    server_response = server_response.get(url)
+    data_of_flight  = server_response.json()
     
-    #checking if there is an error in the website 
-    if 'error' in flight_data:
-        print('>>> Error: '+flight_data['error']['message'])
-        sys.exit
+    #checking if there is an error on the website 
+    if 'error' in data_of_flight :
+        print('>>> Error: '+data_of_flight ['error']['message'])
+        sys.exit()
+    # if there is no data available on the website
+    if data_of_flight['data'] ==[]:
+        print('>>> There is No Data Matching This Airport Code from the [SERVER]')
+        return None
+    with open ('group_B9.json','w') as f:
+        json.dump(data_of_flight , f, indent=4)
+    return data_of_flight    
+# End of retrieve_data function
