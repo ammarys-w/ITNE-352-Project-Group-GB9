3c41c8bb55b8cdf950260291eb636a7d2b5b9f97
ammarys-w <ammar39876@gmail.com>
2023-12-27 10:05:47 +0300

    adding functionn and chcking error in website

--- server_gb9.py
+++ server_gb9.py
@@ -1,8 +1,16 @@
 import socket,json
-
+import sys
 api_key = "8d782c20ade5ab1413c59a3f9b545516"
 IP="192.168.8.101"
 port=40000
 
 # Getting data from the WEbsite
 def get_data(arr_icao):
+    link = f"http://api.aviationstack.com/v1/flights?access_key={api_key}&arr_icao={arr_icao}&limit=100"
+    response = response.get(link)
+    flight_data = response.json()
+    
+    #checking if there is an error in the website 
+    if 'error' in flight_data:
+        print('>>> Error: '+flight_data['error']['message'])
+        sys.exit
