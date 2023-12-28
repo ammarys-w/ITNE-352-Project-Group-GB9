34ddbef1e87d74d1a6d55c7eb03c7e46cc912307
ammarys-w <ammar39876@gmail.com>
2023-12-28 11:57:26 +0300

    Update server_gb9.py

--- server_gb9.py
+++ server_gb9.py
@@ -1,5 +1,6 @@
 import socket,json
 import sys
+
 api_key = "8d782c20ade5ab1413c59a3f9b545516"
 IP="192.168.8.101"
 port=50000
@@ -14,7 +15,7 @@ def retrieve_data(arr_icao):
     if 'error' in data_of_flight :
         print('>>> Error: '+data_of_flight ['error']['message'])
         sys.exit()
-    # if there is no data available on the website
+    # checking if there is no data available on the website
     if data_of_flight['data'] ==[]:
         print('>>> There is No Data Matching This Airport Code from the [SERVER]')
         return None
@@ -28,7 +29,7 @@ def retrieve_data(arr_icao):
 def extract_flight_arrived(data):
     flight_info = []
     for flight in data:
-        info ={
+        info = {
             'Departure Airport': flight['departure']['airport'],
             'Flight IATA': flight['flight']['iata'],
             'Arrival Time': flight['arrival']['Scheduled'],
@@ -43,7 +44,7 @@ def extract_flight_arrived(data):
 def extract_flight_delayed(data):
     delayed_flights = []
     for flight in data:
-        info ={
+        info = {
             'Departure Airport': flight['departure']['airport'],
             'Flight IATA': flight['flight']['iata'],
             'Origin Departure Time': flight['departure']['Scheduled'],
@@ -58,6 +59,7 @@ def extract_flight_delayed(data):
             delayed_flights.append(info)
     return delayed_flights
 # End of extract_flight_delayed function
+
 
 
     
